3044f2a93a11b10aa234662787d203d4148414e7
sn9devil <254550205@qq.com>
2018-07-25 15:27:09 +0800

    sucessful

--- scrapyuniversal/scrapyuniversal/spiders/universal.py
+++ scrapyuniversal/scrapyuniversal/spiders/universal.py
@@ -2,12 +2,15 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from scrapyuniversal.items import *
+from scrapyuniversal.loaders import *
 from scrapyuniversal.utils import get_config
 from scrapyuniversal.rules import rules
 
 
 class UniversalSpider(CrawlSpider):
     name = 'universal'
+
     def __init__(self, name, *args, **kwargs):
         config = get_config(name)
         self.config = config
@@ -16,10 +19,20 @@ class UniversalSpider(CrawlSpider):
         self.allowed_domains = config.get('allowed_domains')
         super().__init__()
 
-
     def parse_item(self, response):
-        i = {}
-        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-        #i['name'] = response.xpath('//div[@id="name"]').extract()
-        #i['description'] = response.xpath('//div[@id="description"]').extract()
-        return i
+        item = self.config.get('item')
+        if item:
+            cls = eval(item.get('class'))()
+            loader = eval(item.get('loader'))(cls, response=response)
+            # 动态获取属性配置
+            for key, value in item.get('attrs').items():
+                for extractor in value:
+                    if extractor.get('method') == 'xpath':
+                        loader.add_xpath(key, *extractor.get('args'), **{'re': extractor.get('re')})
+                    if extractor.get('method') == 'css':
+                        loader.add_css(key, *extractor.get('args'), **{'re': extractor.get('re')})
+                    if extractor.get('method') == 'value':
+                        loader.add_value(key, *extractor.get('args'), **{'re': extractor.get('re')})
+                    if extractor.get('method') == 'attr':
+                        loader.add_value(key, getattr(response, *extractor.get('args')))
+            yield loader.load_item()
